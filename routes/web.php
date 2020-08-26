fec323dfb6be592f2006ecf1cdd12aa7f57359c1
Aleksandr <TamochkinAV@sibintek.ru>
2020-08-26 15:29:32 +0400

    Add auth

--- routes/web.php
+++ routes/web.php
@@ -1,57 +1,62 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index')->with('title', 'Главная');
-});
+Auth::routes();
 
-Route::prefix('pmo')->group(function () {
-    Route::prefix('fileStorage')->group(function () {
-        Route::get('/', 'FileStorageController@index')->name('fileStorageIndex');
-        Route::get('/{folder}', 'FileStorageController@show')->name('fileStorageShow');
-        Route::post('/load', 'FileStorageController@load')->name('fileStorageLoad');
-        Route::any('/download/{path}', 'FileStorageController@download')->name('fileStorageDownload');
-        Route::post('createFolder', 'FileStorageController@createFolder')->name('createFolder');
-        Route::post('editFolder', 'FileStorageController@editFolder')->name('editFolder');
-        Route::post('editFile', 'FileStorageController@editFile')->name('editFile');
-        Route::get('deleteFile/{path}', 'FileStorageController@deleteFile')->name('deleteFile');
-        Route::get('deleteFolder/{path}', 'FileStorageController@deleteFolder')->name('deleteFolder');
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/', function () {
+        return view('index')->with('title', 'Главная');
     });
 
-    Route::prefix('budget')->group(function () {
-        Route::get('/', 'Test@budget')->name('budget');
-    });
+    Route::prefix('pmo')->group(function () {
+        Route::prefix('fileStorage')->group(function () {
+            Route::get('/', 'FileStorageController@index')->name('fileStorageIndex');
+            Route::get('/{folder}', 'FileStorageController@show')->name('fileStorageShow');
+            Route::post('/load', 'FileStorageController@load')->name('fileStorageLoad');
+            Route::any('/download/{path}', 'FileStorageController@download')->name('fileStorageDownload');
+            Route::post('createFolder', 'FileStorageController@createFolder')->name('createFolder');
+            Route::post('editFolder', 'FileStorageController@editFolder')->name('editFolder');
+            Route::post('editFile', 'FileStorageController@editFile')->name('editFile');
+            Route::get('deleteFile/{path}', 'FileStorageController@deleteFile')->name('deleteFile');
+            Route::get('deleteFolder/{path}', 'FileStorageController@deleteFolder')->name('deleteFolder');
+        });
 
-    Route::prefix('project')->group(function () {
-        Route::get('/', 'Test@project')->name('project');
-    });
+        Route::prefix('budget')->group(function () {
+            Route::get('/', 'Test@budget')->name('budget');
+        });
 
-    Route::prefix('contract')->group(function () {
-        Route::get('/', 'Test@contract')->name('contract');
-    });
+        Route::prefix('project')->group(function () {
+            Route::get('/', 'Test@project')->name('project');
+        });
 
-    Route::prefix('library')->group(function () {
-        Route::get('/', 'Test@library')->name('library');
-    });
+        Route::prefix('contract')->group(function () {
+            Route::get('/', 'Test@contract')->name('contract');
+        });
 
-    Route::prefix('maintenance')->group(function () {
-        Route::get('/', 'Test@maintenance')->name('maintenance');
-    });
+        Route::prefix('library')->group(function () {
+            Route::get('/', 'Test@library')->name('library');
+        });
 
-    Route::prefix('scenario')->group(function () {
-        Route::get('/', 'Test@scenario')->name('scenario');
-    });
+        Route::prefix('maintenance')->group(function () {
+            Route::get('/', 'Test@maintenance')->name('maintenance');
+        });
 
-    Route::prefix('report')->group(function () {
-        Route::get('/', 'Test@report')->name('report');
-    });
+        Route::prefix('scenario')->group(function () {
+            Route::get('/', 'Test@scenario')->name('scenario');
+        });
 
-    Route::prefix('document')->group(function () {
-        Route::get('/', 'Test@document')->name('document');
-    });
+        Route::prefix('report')->group(function () {
+            Route::get('/', 'Test@report')->name('report');
+        });
 
-    Route::prefix('integration')->group(function () {
-        Route::get('/', 'Test@integration')->name('integration');
+        Route::prefix('document')->group(function () {
+            Route::get('/', 'Test@document')->name('document');
+        });
+
+        Route::prefix('integration')->group(function () {
+            Route::get('/', 'Test@integration')->name('integration');
+        });
     });
 });
