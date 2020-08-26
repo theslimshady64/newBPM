92765eae2cbfde92e0ccce82c1bda4dda4f9b381
Aleksandr <TamochkinAV@sibintek.ru>
2020-08-26 13:22:03 +0400

    Revert "Add all files"
    This reverts commit 6d7649b91a369864867c2a3cd9ae0fc13e41253b.

--- storage/framework/views/a8c1ae3cc477134f903ce63293725047136d2192.php
+++
@@ -1,6 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <h1 class="mt-3"><?php echo e($title); ?></h1>
-    <hr>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\OSPanel\domains\BPM\resources\views/index.blade.php ENDPATH**/ ?>
